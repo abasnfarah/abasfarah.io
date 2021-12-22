2a0351f6ca4239a2be4bc589387c53b4bb05ac66
Abas Farah <abasnfarah@gmail.com>
2021-12-21 22:45:58 -0600

    Initial Commit with Navbar and logo

---
+++ components/fonts.js
@@ -0,0 +1,11 @@
+import { Global, css } from '@emotion/react'
+
+const Fonts = () => (
+  <Global
+    styles={css`
+      @import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap');
+    `}
+  />
+)
+
+export default Fonts
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import { Box, Container, Heading } from '@chakra-ui/react'
+
 const Page = () => {
-  return <div>Hello</div>
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Hello, I&apos;m a full-stack devloper based in Minneapolis!
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Abas Farah
+          </Heading>
+          <p>Human from Terra 1 (Engineer / Writer / Entrepreneur)</p>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 
 export default Page
